baea353f19b81643bbf8658df1aa0219abd65700
Marvin Kraus <Marvin.Kraus@hotmail.de>
2023-05-17 17:08:52 +0200

    Added get-workspace logic

--- modules/functions/rules-to-workspace/src/index.py
+++ modules/functions/rules-to-workspace/src/index.py
@@ -54,6 +54,7 @@ def get_state_version(state_id):
         if response.status_code == 200:
                 logger.info(f"Successfully got state version {state_id}")
                 return json.loads(response.text)['data']
+        
         else:
                 logger.error(f"Failed to get state version {state_id}")
                 raise Exception(f"Failed to get state version {state_id}")
@@ -65,33 +66,32 @@ def get_state_providers(state_version):
         returns: list of providers
         '''
         logger.info(f"Getting providers from workspace {state_version['relationships']['workspace']['data']['id']}")
-        # Implement logic to transform state version providers into a list of provider names
-        providers = state_version['attributes']['providers']
+        # TODO: Logic to transform state version providers into a list of provider names
+        providers = [provider['name'].split('/')[-1] for provider in state_version['attributes']['providers']]
+        
         return providers
 
-def generate_s3_keys(workspace, provider):
+def find_rule_s3_keys(entity, environment, providers):
         '''
-        Generate S3 keys based on the workspace data
-        params: workspace - workspace data
-                provider - provider to generate keys for
-        returns: list of S3 keys
+        Find rule keys based on the entity and environment
+        params: entity - entity to get rules for
+                environment - environment to get rules for
+        returns: list of rule keys
         '''
-        environment = workspace['environment']
-        entity = workspace['organization']
-        csp = workspace['csp']
-        # TODO: Get CSP from provider list
-
-        # Global rules
-        global_rules_prefix = f'/all/{csp}/'
-        # Entity rules
-        entity_rules_prefix = f'/{entity}/{csp}/{environment}/'
-
-        # Look for s3 objects based on prefix
-        # TODO: Implement logic to check if the object exists
-        global_rules = s3_client.list_objects_v2(Bucket=rules_bucket, Prefix=global_rules_prefix)
-        entity_rules = s3_client.list_objects_v2(Bucket=rules_bucket, Prefix=entity_rules_prefix)
-        rule_keys = []
-        return rule_keys
+        filtered_rule_keys = []
+        # Get all rule keys
+        response = s3_client.list_objects_v2(Bucket=rules_bucket)
+        if 'Contents' in response:
+            for provider in providers:
+                for rule in response['Contents']:
+                        # Filter for rule json files for all entities
+                        if rule['Key'].endswith('.json') and (rule['Key'].startswith(f'ALL/{provider}/ALL/') or rule['Key'].startswith(f'ALL/{provider}/{entity}/')):
+                                filtered_rule_keys.append(rule['Key'])
+                        # Filter for rule json files in specific entities
+                        elif rule['Key'].endswith('.json') and (rule['Key'].startswith(f'{entity}/{provider}/ALL/') or rule['Key'].startswith(f'{entity}/{provider}/{environment}/')):
+                                filtered_rule_keys.append(rule['Key'])
+        
+        return filtered_rule_keys
 
 def lambda_handler(event, context):
         '''
@@ -100,12 +100,14 @@ def lambda_handler(event, context):
         returns: workspace data
         '''
         workspace = event
+        
         # Load state version from Terraform Enterprise API
         state_version = get_state_version(workspace['state_version'])
+        # Extract Provider names from state version
         providers = get_state_providers(state_version)
 
-        # Generate S3 Keys based on workspace data
-        s3_keys = generate_s3_keys(workspace, providers)
+        # Generate S3 Keys based on organization & providers
+        s3_keys = find_rule_s3_keys(workspace, providers)
         if s3_keys != []:
                 workspace['rule_keys'] = s3_keys
 
